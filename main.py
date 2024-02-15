c999a086fcae4e58a02d85241f75df57e922ca84
ericsandev <ericsandev0@gmail.com>
2024-02-15 09:13:36 -0600

    resize

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 import requests
+import cv2
+import numpy as np
+import os
 
-PEXELS_API_KEY = ''
-query = 'sad'
-page = 1
+PEXELS_API_KEY = 'YVKnAcV4qx4wjXk72SdybECg3SayuBFbxO1vajKrn5a0E8MnRARLAJ7z'
+query = 'fitness'
+page = 2
 per_page = 1
-size= 'large'
-orientation='portrait'
+size = 'large'
+orientation = 'portrait'
+desired_width = 1080
+desired_height = 1920
 
 # Hacer la solicitud a la API de Pexels
 url = f'https://api.pexels.com/videos/search?query={query}&per_page={per_page}&page={page}&size={size}&orientation={orientation}'
@@ -23,8 +28,27 @@ if response.status_code == 200:
         # Descargar el video
         r = requests.get(url_video)
         
-        # Guardar el video en un archivo
-        with open(f"{video['id']}.mp4", 'wb') as outfile:
+        # Guardar el video en un archivo temporal
+        with open(f"{video['id']}_temp.mp4", 'wb') as outfile:
             outfile.write(r.content)
+
+        # Redimensionar el video a la resolución deseada
+        cap = cv2.VideoCapture(f"{video['id']}_temp.mp4")
+        fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+        out = cv2.VideoWriter(f"{video['id']}_resized.mp4", fourcc, 30, (desired_width, desired_height))
+
+        while(cap.isOpened()):
+            ret, frame = cap.read()
+            if not ret:
+                break
+            resized_frame = cv2.resize(frame, (desired_width, desired_height))
+            out.write(resized_frame)
+
+        cap.release()
+        out.release()
+        
+        # Eliminar el archivo temporal
+        os.remove(f"{video['id']}_temp.mp4")
+
 else:
     print('Error al realizar la solicitud a la API de Pexels.')
